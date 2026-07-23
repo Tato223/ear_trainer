3bf349134c0100dbc4b45ab0ec73bd96f9584c20
dTravis23 <datra5@morgan.edu>
2026-07-22 23:51:39 -0400

    - initial commit

---
+++ ear_trainer/src/header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header>
+            <h2 className="header-title">EarTrainer</h2>
+
+            <nav className="nav-container">
+                <ul className="nav-links">
+                    <a>Quizzes</a>
+                    <a>Endless Mode</a>
+                    <a>Leaderboard</a>
+                    <button className="signup-button">Sign up</button>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
