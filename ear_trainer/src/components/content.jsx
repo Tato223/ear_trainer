b4a5f2b7a8cdee03cf91ebbc61008d869fc7d9ca
dTravis23 <datra5@morgan.edu>
2026-07-23 16:41:26 -0400

    - Imported Inter font - Adjusted responsiveness in Hero section and Header - Replace placeholder text in Hero section

--- ear_trainer/src/components/content.jsx
+++ ear_trainer/src/components/content.jsx
@@ -2,9 +2,24 @@ function Content() {
     return (
         <div className="content-container">
             <div className="hero-container">
-                <h1 className="hero">Some big headline</h1>
-                <p className="hero__subtitle">add subtitle here</p>
+                <h1 className="hero">Hear Better. <br></br>Play Better.</h1>
+                <p className="hero__subtitle">Develop confident pitch recognition with quizzes, endless challenges, and a complete note library.</p>
                 <button className="cta-button">Get Started</button>
+
+                <hr/>
+            </div>
+
+            <div className="about-container">
+                <h2 className="about-heading">
+                    About
+                </h2>
+
+
+                <p className="about-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsa vel laboriosam dolores eveniet, reiciendis recusandae
+                    nesciunt inventore saepe necessitatibus optio non natus, maiores voluptate totam!
+                </p>
             </div>
         </div>
 
