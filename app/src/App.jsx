8139174b64b231df5c2f46978019f8bc1826ebe7
dTravis23 <datra5@morgan.edu>
2026-07-27 21:30:04 -0400

    - Designed basic signup page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,19 +5,23 @@ import Footer from './components/footer'
 import { createBrowserRouter, Route } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
 
+
+// Page componenents
 import './App.css'
 import Home from './pages/home'
 import Quizzes from './pages/quizzes'
 import EndlessPage from './pages/endless_page'
 import LeaderboardPage from './pages/leaderboard_page'
 import LibraryPage from './pages/library_page'
+import SignupPage from './pages/sign_up_page'
 
 const router = createBrowserRouter([
   {path: "/", element: <Home/>},
   {path: "/quizzes", element: <Quizzes/>},
   {path: "/endless", element: <EndlessPage/>},
   {path: "/leaderboard", element: <LeaderboardPage/>},
-  {path: "/library", element: <LibraryPage/>}
+  {path: "/library", element: <LibraryPage/>},
+  {path: "/signup", element: <SignupPage/>}
 ]);
 
 function App() {
---
+++ app/src/pages/sign_up_page.jsx
@@ -0,0 +1,49 @@
+import Header from "../components/header";
+import Footer from "../components/footer";
+import PropTypes from "prop-types";
+import { Form } from "react-router";
+
+export default function SignupPage() {
+  return (
+    <>
+      <Header />
+      <SignupContent />
+      <Footer />
+    </>
+  );
+}
+
+export function SignupContent() {
+  return (
+    <div className="content-container">
+      <div className="signup-container">
+        <form className="signup-form">
+
+          <FormBlock label="Username" id="username-entry" type="text" min={3} max={12} isRequired={true} placeholder="Enter your username."/>
+          <FormBlock label="Password" id="password-entry" type="text" min={3} max={12} isRequired={true} placeholder="Ex. MyPassword123"/>
+          <FormBlock label="Email" id="email-entry" type="email" min={3} max={12} isRequired={true} placeholder="Ex. yourEmail321@domain.com"/>
+
+          <input className="submit-btn" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export function FormBlock(props) {
+    return(
+        <div className="form-block">
+            <label for="#form-entry">{props.label}</label>
+            <input id={props.id} type="text" min={props.min} max={props.max} required={props.isRequired} placeholder={props.placeholder}></input>
+        </div>
+    );
+}
+
+FormBlock.propTypes = {
+    label: PropTypes.string,
+    id: PropTypes.string,
+    min: PropTypes.number ? PropTypes.number : 3,
+    max: PropTypes.number ? PropTypes.number : 12,
+    isRequired: PropTypes.bool,
+    placeholder: PropTypes.string
+};
