47b75c291681ad4cb9157955725efdb398867641
dTravis23 <datra5@morgan.edu>
2026-07-28 00:28:15 -0400

    Adjusted signup form styling

--- app/src/pages/sign_up_page.jsx
+++ app/src/pages/sign_up_page.jsx
@@ -14,13 +14,26 @@ export default function SignupPage() {
 }
 
 export function SignupContent() {
+
+  /* props = [
+    "label",
+    "text",
+    "id",
+    "max",
+    "min",
+    "placeholder"
+  ] */
+
   return (
     <div className="content-container">
       <div className="signup-container">
+
         <form className="signup-form">
 
+          <h2 className="signup-prompt">Create an Account</h2>
+
           <FormBlock label="Username" id="username-entry" type="text" min={3} max={12} isRequired={true} placeholder="Enter your username."/>
-          <FormBlock label="Password" id="password-entry" type="text" min={3} max={12} isRequired={true} placeholder="Ex. MyPassword123"/>
+          <FormBlock label="Password" id="password-entry" type="password" min={3} max={12} isRequired={true} placeholder="Ex. MyPassword123"/>
           <FormBlock label="Email" id="email-entry" type="email" min={3} max={12} isRequired={true} placeholder="Ex. yourEmail321@domain.com"/>
 
           <input className="submit-btn" type="submit" />
@@ -34,11 +47,12 @@ export function FormBlock(props) {
     return(
         <div className="form-block">
             <label for="#form-entry">{props.label}</label>
-            <input id={props.id} type="text" min={props.min} max={props.max} required={props.isRequired} placeholder={props.placeholder}></input>
+            <input id={props.id} type={props.type} min={props.min} max={props.max} required={props.isRequired} placeholder={props.placeholder}></input>
         </div>
     );
 }
 
+// Define datatypes for component props
 FormBlock.propTypes = {
     label: PropTypes.string,
     id: PropTypes.string,
