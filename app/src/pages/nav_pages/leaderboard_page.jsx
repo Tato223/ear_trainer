a6c41c74908f8cd304afc788fb3fa4be6c1a28a9
dTravis23 <datra5@morgan.edu>
2026-08-16 23:48:00 -0400

    - implemented hero page image and styling adjustments

--- app/src/pages/nav_pages/leaderboard_page.jsx
+++ app/src/pages/nav_pages/leaderboard_page.jsx
@@ -70,7 +70,7 @@ export function LeaderboardEntry(props) {
       <h2 className="leaderboard__position">{props.position}</h2>
       <img className="leaderboard__pfp" src="https://placehold.co/60" />
       <h3 className="leaderboard__username">{props.name}</h3>
-      <h2 className="leaderboard__points">{props.points}</h2>
+      <h2 className="leaderboard__points">{props.points} points</h2>
     </div>
   );
 }
