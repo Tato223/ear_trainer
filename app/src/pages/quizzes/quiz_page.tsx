93fb2f0f77b22d05cba3cc8120fc37ac94217372
dTravis23 <datra5@morgan.edu>
2026-09-08 12:40:27 -0400

    -converted speaker button jsx into a SpeakerButton component

--- app/src/pages/quizzes/quiz_page.tsx
+++ app/src/pages/quizzes/quiz_page.tsx
@@ -6,6 +6,7 @@ import { PitchRecognitionQuestion, NaturalNote } from "../../types.ts";
 import { useEffect, useState } from "react";
 import * as tone from "tone";
 import { Navigate } from "react-router";
+import SpeakerButton from "../../components/speaker_button.jsx";
 
 const maxPitchRecognitionQuestions = 10;
 const noteLength = "4n"; //Quarter note
@@ -95,22 +96,7 @@ export function QuizPageContent() {
         onClick={selectedAnswer ? handleAnswer : null} />
       </div>
 
-      <div className="quiz-img-container" onClick={toggleIsPlaying}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 -960 960 960"
-          width="24px"
-          fill="#e3e3e3"
-        >
-          <path
-            d="M560-131v-82q90-26 145-100t55-168q0-94-55-168T560-749v-82q124
-            28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm440
-            40v-322q47 22 73.5 66t26.5 96q0 51-26.5
-            94.5T560-320ZM400-606l-86 86H200v80h114l86 86v-252ZM300-480Z"
-          />
-        </svg>
-      </div>
+      <SpeakerButton onClick={toggleIsPlaying}/>
 
       <div className="quiz-options-container">
         {currentQuestion.options.map((note) => (
