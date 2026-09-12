b3691983e26cced6ecd44543c70f390fd3e1f348
dTravis23 <datra5@morgan.edu>
2026-09-12 14:36:58 -0400

    added audio_config.ts to maintain audio playback functions
    adjusted quiz_page.tsx to utilize audio config functions depending on the quiz type prop

---
+++ app/src/audio_config.ts
@@ -0,0 +1,77 @@
+import * as tone from "tone";
+import * as types from "./types.ts";
+
+export function playSingleNote(
+  currentQuestion: any,
+  setIsPlaying: Function,
+  defaultOctave: types.octave,
+  noteLength: types.NoteLength,
+) {
+  const synth = new tone.Synth().toDestination();
+  synth.triggerAttackRelease(
+    currentQuestion.correctAnswer + defaultOctave,
+    `${noteLength}n`,
+  );
+
+  return () => {
+    synth.dispose();
+    setIsPlaying(false);
+  };
+}
+
+export function playScale(
+  seenBorBb: types.Note[],
+  seenNotes: types.Note[],
+  note: types.Note,
+  prevOrderedIndex: number | null,
+  index: number,
+  synth: tone.Synth,
+  defaultOctave: types.octave,
+  noteLength: types.NoteLength,
+): void {
+  let octave = defaultOctave;
+  let currOrderedIndex = types.orderedNotes.indexOf(note);
+  const indexOfBb = 15;
+
+  if (
+    (prevOrderedIndex && currOrderedIndex < prevOrderedIndex) ||
+    (currOrderedIndex < indexOfBb && seenBorBb.length > 0) ||
+    seenNotes.includes(note)
+  ) {
+    octave++;
+  } else {
+    octave = defaultOctave;
+  }
+
+  prevOrderedIndex = currOrderedIndex;
+  const scheduledTime = tone.now() + index * 0.25;
+
+  synth.triggerAttackRelease(`${note}${octave}`, noteLength, scheduledTime);
+
+  if (note === "B" || note === "Bb") {
+    seenBorBb.push(note);
+  }
+
+  seenNotes.push(note);
+}
+
+export function playInterval(
+    note: types.Note,
+    prevOrderedIndex: number | null, 
+    index: number, 
+    octave: types.octave, 
+    synth: tone.Synth, 
+    noteLength: types.NoteLength, 
+    delayBetweenNotes: number = 0.75) {
+  let currOrderedIndex = types.orderedNotes.indexOf(note);
+  prevOrderedIndex && currOrderedIndex < prevOrderedIndex ? octave++ : null;
+
+  const scheduledTime = tone.now() + index * delayBetweenNotes;
+  synth.triggerAttackRelease(`${note}${octave}`, noteLength, scheduledTime);
+
+  prevOrderedIndex = currOrderedIndex;
+}
+
+// playIntonationNote() {
+    
+// }
--- app/src/pages/quizzes/quiz_page.tsx
+++ app/src/pages/quizzes/quiz_page.tsx
@@ -2,88 +2,147 @@ import Header from "../../components/header.jsx";
 import Footer from "../../components/footer.jsx";
 import SubmitQuestionButton from "../../components/submit_question_btn.jsx";
 import * as quizLogic from "../../quiz_logic.ts";
-import { PitchRecognitionQuestion, NaturalNote } from "../../types.ts";
+import * as types from "../../types.ts";
 import { useEffect, useState } from "react";
 import * as tone from "tone";
 import { Navigate } from "react-router";
 import SpeakerButton from "../../components/speaker_button.jsx";
-
-const maxPitchRecognitionQuestions = 10;
-const noteLength = "4n"; //Quarter note
+import * as audioConfig from "../../audio_config.ts";
 
 export default function QuizPage() {
   return (
     <>
       <Header />
-      <QuizPageContent/>
+      <QuizPageContent
+        quizType={types.PitchRecognitionQuiz}
+        maxQuestions={10}
+      />
       <Footer />
     </>
   );
 }
 
-export function QuizPageContent() {
-  let questionText = "Select an answer choice to identify the note.";
+export function QuizPageContent({
+  quizType,
+  maxQuestions = 10,
+}: types.QuizProps) {
   const [questionsCompleted, setQuestionsCompleted] = useState<number>(1);
-
-  const [currentQuestion, setCurrentQuestion] =
-    useState<PitchRecognitionQuestion>(() =>
-      quizLogic.createPitchRecognitionQuestion(questionText),
-    );
-
+  const [questionsCorrect, setQuestionsCorrect] = useState<number>(0);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [selectedAnswer, setSelectedAnswer] = useState<
+    typeof quizType.optionsType | null
+  >(null);
 
-  const [selectedAnswer, setSelectedAnswer] = useState<NaturalNote | null>(
-    null,
+    const questionCreationFunctions = {
+    PitchRecognitionQuiz: quizLogic.createPitchRecognitionQuestion,
+    MajorScalesQuiz: quizLogic.createMajorScaleQuestion,
+    IntervalsQuiz: quizLogic.createIntervalQuestion,
+    IntonationQuiz: quizLogic.createIntonationQuestion,
+  };
+
+  type QuizName = keyof typeof questionCreationFunctions;
+
+  function getQuestionCreationFunction(quizName: QuizName): any {
+    if (quizName in questionCreationFunctions) {
+      return questionCreationFunctions[quizName];
+    }
+  }
+
+  let createQuestion = getQuestionCreationFunction(quizType.name);
+  
+  const [currentQuestion, setCurrentQuestion] = useState<any>(() =>
+    createQuestion(quizType.questionText),
   );
 
   // play correct pitch
   useEffect(() => {
-    const synth = new tone.Synth().toDestination();
-    synth.triggerAttackRelease(currentQuestion.correctAnswer + "4", noteLength);
+    if (quizType.name == "PitchRecognitionQuiz") {
+      audioConfig.playSingleNote(
+        currentQuestion,
+        setIsPlaying,
+        quizType.defaultOctave,
+        quizType.noteLength,
+      );
+    }
 
-    return () => {
-      synth.dispose();
-      setIsPlaying(false);
-    };
+    if (quizType.name === "MajorScalesQuiz") {
+      const synth = new tone.Synth().toDestination();
+      const correctScaleNotes: types.Note[] =
+        currentQuestion.correctAnswer.Notes;
+
+      let prevOrderedIndex: number | null = null;
+      const seenBorBb: types.Note[] = [];
+      const seenNotes: types.Note[] = [];
+
+      correctScaleNotes.forEach((note: types.Note, index: number) => {
+        audioConfig.playScale(
+          seenBorBb,
+          seenNotes,
+          note,
+          prevOrderedIndex,
+          index,
+          synth,
+          quizType.defaultOctave,
+          quizType.noteLength,
+        );
+      });
+    }
+
+    if (quizType.name === "IntervalsQuiz") {
+      const synth = new tone.Synth().toDestination();
+      const intervalNotes: types.Note[] = currentQuestion.intervalNotes;
+      let octave: types.octave = quizType.defaultOctave;
+      let prevOrderedIndex: null | number = null;
+
+      intervalNotes.forEach((note, index) => {
+        audioConfig.playInterval(
+          note,
+          prevOrderedIndex,
+          index,
+          octave,
+          synth,
+          quizType.noteLength,
+        );
+      });
+    }
+
+    // Add expressions for all other playback function types
   }, [currentQuestion, isPlaying]);
-
-  const [questionsCorrect, setPitchRecognitionQuestionsCorrect] =
-    useState<number>(0);
 
   const quizData = {
     correct: questionsCorrect,
-    numQuestions: maxPitchRecognitionQuestions,
-    quizEndpoint: "/pitch_recognition",
+    numQuestions: maxQuestions,
+    quizEndpoint: quizType.quizEndpoint,
   };
 
   // Navigate to complete page if max questions have been reached
-  if (questionsCompleted > maxPitchRecognitionQuestions) {
-    return <Navigate to="/quizzes/complete" replace={true} state={quizData} />;
+  if (questionsCompleted > maxQuestions) {
+    return (
+      <Navigate to={"/quizzes/complete/"} replace={true} state={quizData} />
+    );
   }
 
-  //TODO: Change logic to only check correctness after a submit
   function handleAnswer() {
     if (selectedAnswer === currentQuestion.correctAnswer) {
-      setPitchRecognitionQuestionsCorrect(questionsCorrect + 1);
+      setQuestionsCorrect(questionsCorrect + 1);
     }
 
     setQuestionsCompleted(questionsCompleted + 1);
-
-    setCurrentQuestion((prev) => ({
+    setCurrentQuestion((prev: any) => ({
       ...prev,
       isAnswered: true,
       selectedAnswer: selectedAnswer,
     }));
 
     nextQuestion();
-
     setSelectedAnswer(null);
   }
 
   function nextQuestion() {
-    setCurrentQuestion(quizLogic.createPitchRecognitionQuestion(questionText));
+    setCurrentQuestion(
+      quizLogic.createPitchRecognitionQuestion(quizType.questionText),
+    );
   }
-
   function toggleIsPlaying() {
     isPlaying ? setIsPlaying(false) : setIsPlaying(true);
   }
@@ -92,24 +151,39 @@ export function QuizPageContent() {
     <div className="content-container">
       <div className="quiz-text__container">
         <h2 className="quiz-text">{currentQuestion.text}</h2>
-        <SubmitQuestionButton className={selectedAnswer? "question-submit-btn-active" : "question-submit-btn-inactive"}
-        onClick={selectedAnswer ? handleAnswer : null} />
+        <SubmitQuestionButton
+          className={
+            selectedAnswer
+              ? "question-submit-btn-active"
+              : "question-submit-btn-inactive"
+          }
+          onClick={selectedAnswer ? handleAnswer : null}
+        />
       </div>
 
-      <SpeakerButton onClick={toggleIsPlaying}/>
+      <SpeakerButton onClick={toggleIsPlaying} />
 
       <div className="quiz-options-container">
-        {currentQuestion.options.map((note) => (
+        {currentQuestion.options.map((option: any) => (
           <button
-            key={note}
+            key={option}
             className={
-              note === selectedAnswer ? "quiz-option-btn__selected" : "quiz-option-btn"}
-            onClick={() => selectedAnswer === note? setSelectedAnswer(null) : setSelectedAnswer(note)}
+              option === selectedAnswer
+                ? "quiz-option-btn__selected"
+                : "quiz-option-btn"
+            }
+            onClick={() =>
+              selectedAnswer === option
+                ? setSelectedAnswer(null)
+                : setSelectedAnswer(option)
+            }
           >
-            {note}
+            {option.toString()}
           </button>
         ))}
       </div>
     </div>
   );
 }
+
+// Make variants for different playback types, then assign the audio function based on the quizType above
